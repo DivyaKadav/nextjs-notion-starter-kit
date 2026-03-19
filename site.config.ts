17db91eba6b11258dd6d5e131403dbcf07554f0b
Divya <115348205+DivyaKadav@users.noreply.github.com>
2026-03-19 11:09:57 +0530

    Update domain in site configuration

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'd828cf2500128373baae018461e5f889',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
