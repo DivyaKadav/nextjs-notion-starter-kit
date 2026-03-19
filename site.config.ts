bb9e87cd9b1dea933d4b10ce8b92201725ea1b77
Divya <115348205+DivyaKadav@users.noreply.github.com>
2026-03-19 11:16:24 +0530

    Modify site configuration for new author and details
    Updated site configuration with new author, domain, and description.

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd828cf2500128373baae018461e5f889',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'd828cf2500128373baae018461e5f889',
-  author: 'Travis Fischer',
+  name: 'Divya Kadav',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Divya Kadav',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+description: 'Portfolio of Divya Kadav | Linguistics, Translation, Japanese, and Research Projects',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+// social usernames (optional)
+github: 'DivyaKadav',
+linkedin: 'kadavdivya',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
